e83cbb3dd9a197b1a62af4a5848e3a2786d9ab0a
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-07-17 12:34:16 +0200

    update js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ const searchBtn = document.querySelector('.search-btn');
 const searchForm = document.querySelector(`.search-form`);
 const inputField = document.querySelector(`.input-field`);
 const resultsGalleryContainer = document.querySelector(`.gallery-result`);
-const imageContainer = document.querySelector('.gallery-result > img');
+const resultsGalleryList = document.querySelector(`.gallery-result-list`);
+const loadText = document.querySelector('.loading-text');
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
@@ -18,46 +19,77 @@ searchForm.addEventListener('submit', event => {
 });
 
 function fetchImages(request) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
+    request
+  )}&image_type=photo&orientation=horizontal&safesearch=true`;
 
+  Loader.enable();
   fetch(url)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .then(data => displayImages(data.hits))
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`Error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(data => {
+      Loader.disable();
+      if (data.hits) {
+        displayImages(data.hits);
+      } else {
+        iziToast.warning({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          backgroundColor: '#ef4040',
+          messageColor: '#fff',
+        });
+      }
+    })
     .catch(error => {
+      Loader.disable();
       console.error(error);
+      iziToast.warning({
+        title: 'Error',
+        message: 'An error occurred while fetching images',
+      });
     });
 }
-
-const Loader = {
-  enable: () => document.querySelector('.spinner').classList.remove('disabled'),
-  disable: () => document.querySelector('.spinner').classList.add('disabled'),
-};
 
 const message =
   'Sorry, there are no images matching your search query. Please try again!';
 
 function displayImages(images) {
-  resultsGalleryContainer.innerHTML = '';
+  resultsGalleryList.innerHTML = '';
   if (images.length === 0) {
     iziToast.warning({
-      message,
+      message: message,
       backgroundColor: '#ef4040',
       messageColor: `#fff`,
     });
     return;
   }
-  images.forEach(image => {
-    const imgElement = document.createElement('img');
-    imgElement.src = image.webformatURL;
-    imgElement.alt = image.tags;
-    resultsGalleryContainer.appendChild(imgElement);
-  });
+  const imagesMarkup = images.map(makeImgItem).join('');
+  resultsGalleryList.insertAdjacentHTML('beforeend', imagesMarkup);
 }
 
-const loadText = document.querySelector('.loading-text');
+function makeImgItem({
+  webformatURL,
+  tags,
+  downloads,
+  likes,
+  comments,
+  views,
+}) {
+  return `<li class="list-container">
+    <div class="image-container">
+      <img src="${webformatURL}" alt="${tags}" />
+      <div class="descr-element">
+        <p>"views ${views} dowmloads ${downloads} likes ${likes} comments ${comments}"</p>
+      </div>
+    </div>
+  </li>`;
+}
 
-//najpierw dodać elementy do DOM
+// galleryList.addEventListener('click', onImgClick);
 
 const details = {
   captions: true,
@@ -70,41 +102,16 @@ const details = {
   overlay: true,
 };
 
-const lightBox = new SimpleLightbox('.gallery-result img', details);
+const lightBox = new SimpleLightbox(
+  '.gallery-result-list ul li div img',
+  details
+);
+
+const Loader = {
+  enable: () => document.querySelector('.spinner').classList.remove('disabled'),
+  disable: () => document.querySelector('.spinner').classList.add('disabled'),
+};
 
 const options = {
   method: 'GET',
 };
-
-// fetch('${ url }${ APIkey }', options)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-//   .then(data => {})
-//   .catch(error => {});
-
-const isSuccess = true;
-// Create promise
-const promise = new Promise((resolve, reject) => {
-  console.log('Loadning images');
-  loadText.classList.remove('disabled');
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve('Success! Value passed to resolve function');
-    } else {
-      reject('Error! Error passed to reject function');
-    }
-  }, 4000);
-});
-
-// Registering promise callbacks
-promise
-  .then(value => {
-    console.log(value); // "Success! Value passed to resolve function"
-  })
-  .catch(error => {
-    console.log(error); // "Error! Error passed to reject function"
-  });
